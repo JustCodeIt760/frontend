afe22213cd761650bc5364bbf02df7e76e70987e
Jake Mann <Jake@BigSkyCapital.com>
2023-11-24 11:17:34 -0800

    testing

--- PCAApp/screens/ForgotPasswordScreen.js
+++ PCAApp/screens/ForgotPasswordScreen.js
@@ -8,14 +8,13 @@ export default function ForgotPasswordScreen() {
 
     const sendResetLink = async () => {
         try {
-            // Correctly concatenate API_ENDPOINT with the endpoint path
             const response = await axios.post(`${API_ENDPOINT}/api/user/password-reset/`, { email });
             console.log("Password reset response:", response.data);
         } catch (error) {
-            // Make sure to handle the case where error.response is undefined
-            console.error("Error during password reset:", error.response ? error.response.data : error);
+            console.error("Error during password reset:", error.response.data);
         }
     };
+
 
 
     return (
